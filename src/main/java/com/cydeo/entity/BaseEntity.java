9f5f4a95eae6457d89fb8da77b82aa86b9680054
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-09-20 11:45:42 +0300

    Initial commit

---
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.cydeo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@MappedSuperclass
+public class BaseEntity {
+    
+    private Long id;
+    private LocalDateTime insertDateTime;
+    private Long insertUserId;
+    private LocalDateTime lastUpdateDateTime;
+    private Long lastUpdateUserId;
+
+}
