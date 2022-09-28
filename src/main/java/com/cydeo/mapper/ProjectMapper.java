ddbd9b606a817b0455b022777ac08494b23cd082
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-09-28 22:20:36 +0300

    Part 3

--- src/main/java/com/cydeo/mapper/ProjectMapper.java
+++ src/main/java/com/cydeo/mapper/ProjectMapper.java
@@ -1,5 +1,23 @@
 package com.cydeo.mapper;
 
-public class ProjectMapper {
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.entity.Project;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
+@Component
+public class ProjectMapper {
+    private final ModelMapper modelMapper;
+
+    public ProjectMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public Project convertToEntity(ProjectDTO dto) {
+        return modelMapper.map(dto, Project.class);
+    }
+
+    public ProjectDTO convertToEntity(Project entity) {
+        return modelMapper.map(entity, ProjectDTO.class);
+    }
 }
